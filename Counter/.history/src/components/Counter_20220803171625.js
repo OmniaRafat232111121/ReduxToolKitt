d828f90c80a78281190fad50688e908848656d6a
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-08-03 17:17:33 +0200

    lists

---
+++ Counter/.history/src/components/Counter_20220803171625.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector ,useDispatch} from 'react-redux/es/exports';
+//import { INCREMENTACTION,DECREMENTACTION,INC_VALUEACTION} from '../redux/actions/countertypes';
+import { decrement, increment, incrementByAmount } from '../redux/counterSlice';
+
+export default function Counter()  {
+    const countState=useSelector(state=>state.counter);
+    const dispatch=useDispatch()
+    console.log(countState)
+    const handleIncrement=()=>{
+        dispatch(increment());
+    }
+    const handleDecrement=()=>{
+         dispatch(decrement());
+    }
+    const handleIncrementByValue=(v1)=>{
+         dispatch(incrementByAmount({pa}))
+    }
+  return (
+    <div>
+
+    <button onClick={handleIncrement}>+</button>
+    <p>{countState}</p>
+
+    <button onClick={handleDecrement}>-</button>
+    <br/>
+    <br/>
+    <button onClick={()=>handleIncrementByValue(8)}>Increment By 8</button>
+    </div>
+  )
+}
